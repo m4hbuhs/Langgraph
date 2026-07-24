32b8a12f1a453ba181227ce46997a9f596928782
m4hbuhs <itsshubham206@gmail.com>
2026-07-24 22:14:10 +0530

    added sqlite in chatbot

--- Langgraph-chatbot/langgraph_database_backend.py
+++ Langgraph-chatbot/langgraph_database_backend.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel,Field
 from dotenv import load_dotenv
 from typing import Literal,TypedDict,Annotated
 from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import MemorySaver
-
+from langgraph.checkpoint.sqlite import SqliteSaver
+import sqlite3
 load_dotenv()
 class Chatstate(TypedDict):
     messages :Annotated[list[BaseMessage],add_messages]
@@ -18,7 +18,9 @@ def chat_node(state : Chatstate) ->Chatstate:
     response = llm.invoke(messages)
 
     return {'messages': [response]}
-checkpointer = MemorySaver()
+
+conn=sqlite3.connect(database='chatbot.db',check_same_thread=False)
+checkpointer = SqliteSaver(conn=conn)
 graph = StateGraph(Chatstate)
 
 graph.add_node('chat_node',chat_node)
@@ -26,3 +28,10 @@ graph.add_edge(START,'chat_node')
 graph.add_edge('chat_node',END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
+
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+
+    return list(all_threads)
